aa388cb0867c66b331310aadcacd781929d6e34e
pk5ls20 <pk5ls20@outlook.com>
2025-02-08 08:04:08 +0800

    chore: add git hook scripts

--- mania/build.rs
+++ mania/build.rs
@@ -1,12 +1,20 @@
+use anyhow::Result;
 use prost_build::Config;
-fn main() {
-    let protos = glob::glob("src/core/protos/**/*.proto")
-        .unwrap()
+use std::path::Path;
+
+fn main() -> Result<()> {
+    if Path::new("../.git").is_dir() {
+        if !Path::new("../.git/hooks").exists() {
+            std::fs::create_dir_all(".././.git/hooks")?;
+        }
+        std::fs::copy("../scripts/pre-commit", "../.git/hooks/pre-commit")?;
+        std::fs::copy("../scripts/pre-push", "../.git/hooks/pre-push")?;
+    }
+    let protos = glob::glob("src/core/protos/**/*.proto")?
         .map(|x| x.unwrap())
         .collect::<Vec<_>>();
     let mut config = Config::new();
-    config
+    Ok(config
         .include_file("_includes.rs")
-        .compile_protos(&protos, &["src/core/protos"])
-        .unwrap();
+        .compile_protos(&protos, &["src/core/protos"])?)
 }
