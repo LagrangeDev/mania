a0cd254ed02df50b8e24aab4c24736e316019216
包布丁 <bdbaiapp@163.com>
2025-06-26 22:26:00 +0800

    feat: add forward impl for AsyncPureStreamTrait (#59)

--- mania/src/core/highway.rs
+++ mania/src/core/highway.rs
@@ -14,6 +14,7 @@ use tokio::io::{AsyncRead, AsyncSeek};
 pub trait AsyncReadSeek: AsyncRead + AsyncSeek {}
 impl<T: AsyncRead + AsyncSeek> AsyncReadSeek for T {}
 pub trait AsyncPureStreamTrait: AsyncReadSeek + Send + Sync + Unpin {}
+impl<T: AsyncReadSeek + Send + Sync + Unpin> AsyncPureStreamTrait for T {}
 pub type AsyncPureStream = Box<dyn AsyncReadSeek + Send + Sync + Unpin>;
 pub type AsyncStream = Arc<tokio::sync::Mutex<AsyncPureStream>>;
 
